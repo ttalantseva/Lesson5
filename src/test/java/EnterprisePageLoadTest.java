892ba6c62064851bf9899fc3399ccac65e8374ab
Tamara Talantseva <t.talanceva@yandex.ru>
2025-01-04 23:54:58 +0700

    renamed class

--- src/test/java/EnterprisePageLoadTest.java
+++ src/test/java/EnterprisePageLoadTest.java
@@ -1,16 +1,14 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class enterprisePageLoadTest {
+public class EnterprisePageLoadTest {
 
 
 
